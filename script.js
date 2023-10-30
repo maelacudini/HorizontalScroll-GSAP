b4d4defd5771470c7defd4567d61070561e179c0
maelacudini <maelacudini@gmail.com>
2023-10-30 12:35:04 +0100

    first commit

--- script.js
+++ script.js
@@ -1,24 +1,38 @@
-// Function to apply GSAP horizontal scroll when the display width is over 800px
-function applyHorizontalScroll() {
-    gsap.registerPlugin(ScrollTrigger);
-    let sections = gsap.utils.toArray(".panel");
-    gsap.to(sections, {
-        xPercent: -100 * (sections.length - 1),
-        ease: "none",
-        scrollTrigger: {
-            trigger: ".container",
-            pin: true,
-            scrub: 1,
-            snap: 1 / (sections.length - 1),
-            end: () => "+=" + (sections.length - 1) * window.innerWidth,
-        },
-    });
-}
+// // Function to apply GSAP horizontal scroll when the display width is over 800px
+// function applyHorizontalScroll() {
+//     gsap.registerPlugin(ScrollTrigger);
+//     let sections = gsap.utils.toArray(".panel");
+//     gsap.to(sections, {
+//         xPercent: -100 * (sections.length - 1),
+//         ease: "none",
+//         scrollTrigger: {
+//             trigger: ".container",
+//             pin: true,
+//             scrub: 1,
+//             snap: 1 / (sections.length - 1),
+//             end: () => "+=" + (sections.length - 1) * window.innerWidth,
+//         },
+//     });
+// }
 
-// Check the window width and apply the effect if it's over 800px
-if (window.innerWidth > 800) {
-    applyHorizontalScroll();
-}
+// // Check the window width and apply the effect if it's over 800px
+// if (window.innerWidth > 800) {
+//     applyHorizontalScroll();
+// }
+
+gsap.registerPlugin(ScrollTrigger);
+let sections = gsap.utils.toArray(".panel");
+gsap.to(sections, {
+    xPercent: -100 * (sections.length - 1),
+    ease: "none",
+    scrollTrigger: {
+        trigger: ".container",
+        pin: true,
+        scrub: 1,
+        snap: 1 / (sections.length - 1),
+        end: () => "+=" + (sections.length - 1) * window.innerWidth,
+    },
+});
 
 // Lenis initialization (always active)
 const lenis = new Lenis();
